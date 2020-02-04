1c47f6a87d3a25cc0fc18b675f22bc08c0acfd0c
farzaneh <fmahdisoltani@gmail.com>
2020-02-04 15:26:31 +0900

    Add MixtureAccumulator class for gmm

--- torchsso/utils/accumulator.py
+++ torchsso/utils/accumulator.py
@@ -55,3 +55,48 @@ class TensorAccumulator(object):
     def clear(self):
         self._accumulation = None
 
+
+class MixtureAccumulator(object):
+
+    def __init__(self, num_gmm_components):
+        self._accumulation = None
+        self.num_gmm_components = num_gmm_components
+
+    def update(self, data, scale=1.):
+        if isinstance(data, list):
+            if self._accumulation is None:
+                self._accumulation = [[None for _ in range(self.num_gmm_components)] for _ in range(len(data))]
+
+            for d_ind in range(len(data)):
+                for comp in range(self.num_gmm_components):
+                    if self._accumulation[d_ind][comp] is None:
+                        self._accumulation[d_ind][comp] = data[d_ind][comp].clone()
+                    else:
+                        self._accumulation[d_ind][comp].add_(data[d_ind][comp])
+
+        else:
+            if self._accumulation is None:
+                self._accumulation = data.mul(scale)
+            else:
+                self._accumulation = self._accumulation.add(scale, data)
+
+    def get(self, clear=True):
+        accumulation = self._accumulation
+        if accumulation is None:
+            return
+
+        data = [[None for _ in range(self.num_gmm_components)] for _ in accumulation]
+
+        if isinstance(accumulation, list):
+            data = [[d.clone() for d in d_list] for d_list in self._accumulation]
+        else:
+            data = accumulation.clone()
+
+        if clear:
+            self.clear()
+
+        return data
+
+    def clear(self):
+        self._accumulation = None
+
